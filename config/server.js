1d70aa211b373ad55c30f065534dece17225fede
Felipe Vasques <felipevc13@gmail.com>
2020-10-26 10:11:28 -0300

    primeiro commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b14ef4875f5214cd4a7aa2060a5759be'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '08b15be6-4b65-438a-95e5-163c044b05b4'
+};
